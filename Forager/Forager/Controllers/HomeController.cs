5b5720a7a568810a138cd41e8c37a192d880f5e2
Jake <wickerbough@gmail.com>
2012-10-02 23:41:34 -0400

    Uploaded the website solution

---
+++ Forager/Forager/Controllers/HomeController.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Forager.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult errors_content()
+        {
+            return View();
+        }
+        public ActionResult errors_deadlinks()
+        {
+            return View();
+        }
+        public ActionResult errors_incpointer()
+        {
+            return View();
+        }
+        public ActionResult errors_menu()
+        {
+            return View();
+        }
+        public ActionResult errors_wrongpic()
+        {
+            return View();
+        }
+        public ActionResult errors()
+        {
+            return View();
+        }
+        public ActionResult heirarchy()
+        {
+            return View();
+        }
+        public ActionResult heirarchy_content()
+        {
+            return View();
+        }
+        public ActionResult heirarchy_download()
+        {
+            return View();
+        }
+        public ActionResult heirarchy_menu()
+        {
+            return View();
+        }
+        public ActionResult member()
+        {
+            return View();
+        }
+        public ActionResult non_domain()
+        {
+            return View();
+        }
+        public ActionResult other()
+        {
+            return View();
+        }
+        public ActionResult priority()
+        {
+            return View();
+        }
+        public ActionResult record1()
+        {
+            return View();
+        }
+        public ActionResult record2()
+        {
+            return View();
+        }
+        public ActionResult record3()
+        {
+            return View();
+        }
+        public ActionResult records()
+        {
+            return View();
+        }
+        public ActionResult records_content()
+        {
+            return View();
+        }
+        public ActionResult records_menu()
+        {
+            return View();
+        }
+        public ActionResult ssl()
+        {
+            return View();
+        }
+        public ActionResult ssl_content()
+        {
+            return View();
+        }
+        public ActionResult ssl_https()
+        {
+            return View();
+        }
+        public ActionResult ssl_menu()
+        {
+            return View();
+        }
+    }
+}
---
+++ Forager/Forager/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Forager.MvcApplication" Language="C#" %>
